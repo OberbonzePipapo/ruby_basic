6654fa989d185b585331bf5469cfcb04f184d3cc
Elina Eickstaedt <contact@khaleesicodes.com>
2019-10-14 10:14:57 +0200

    Updated documentation

--- Augabe01/aufgabe_01.rb
+++ Augabe01/aufgabe_01.rb
@@ -4,8 +4,17 @@
 puts "Hi, welcome to the first Ruby assignment! Lets mix up some strings, please enter a string here!"
 
 input = gets.chomp
+#reversing the entered word
 puts(input.reverse)
+
+# converting everything into small letters
 puts(input.downcase)
+
+#convertig everything into uppercase letters
 puts(input.upcase)
+
+# substitute aeiou with *
 puts(input.gsub(/[aeiou]/, '*'))
+
+# Encrypt word with the code of the white rose
 puts(input.gsub(/[[b-zäöüß]&&[^eiou]]/i, '\0o\0'))
--- Augabe01/aufgabe_02.rb
+++ Augabe01/aufgabe_02.rb
@@ -1,14 +1,11 @@
 # Program to read, write and open files
 # Author:: Elina Eickstaedt
-# Gross und Kleinschreibung eleminieren => alles klein
-# Entfernen von Wortzeichen
-# Wortzeichen umdrehen (reverse) ein Wort pro zeile => split.join("\n")
-# Anzahl mehrfach vorkommender Worte zählen
 
 puts("Geben Sie den Dateipfad fuer die Datei ein die Sie bearbeiten moechten!")
 input_file = gets.chomp
 
-#2.1 Text einlesen, hier Text auch in einen array schreiben um spaeter damit arbeiten zu koennen
+#2.1 Read access on the file, putting it out on the command line.
+# Saved all lines of the file in an array to later work with it
 
 lines = []
 
@@ -19,16 +16,17 @@ File.open(input_file, 'r:UTF-8') do |file|
   end
 end
 
-#2.2
+#2.2 Created new array and saved all word seperated from each other in downcases, and delted non wordsigns
 words = []
 
 lines.each do |line|
   words.concat(line.downcase.scan(/[a-zäöüß]+/))
 end
 
+#kept in code in case of testing later
 #puts(words)
 
-# 2.3
+# 2.3 Reversed all words and saved it in a new output file
 
 File.open("data/output.txt", 'w') do |file|
   words.each do |word|
@@ -36,14 +34,19 @@ File.open("data/output.txt", 'w') do |file|
   end
 end
 
+
+#kept in code in case of testing later
 #puts(File.open("data/output.txt", 'r').read)
 
-#2.4
+#2.4 Here words get counted accordingly to how often they are appering in the file. And printing it sorted on the command line
 
+#Hash that counts words
 words_counter = Hash.new(0)
 
+# Count words
 words.each do |word|
   words_counter[word] += 1
 end
 
+#print out sorted
 words_counter.sort_by{ |_k, v| v }.each { |word, count| puts("#{word} kommt #{count} vor") }
