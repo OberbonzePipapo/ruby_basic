e1d31f69845a465bb448bdc10b44ad2d7919bb34
Elina Eickstaedt <contact@khaleesicodes.com>
2019-10-20 15:46:49 +0200

    Corrected break statement in 0303

--- Aufgabe02/aufgabe0303.rb
+++ Aufgabe02/aufgabe0303.rb
@@ -14,17 +14,17 @@ end
 def aprox_one
   #fix start value of the conjecture is 2 => k = 2
   k = 2
-  #variable to sve the continuing value
+  #variable to save the continuing value
   current_value = 0.0
   #variable to save the difference of one an the current value, also break argument
-  div_conv = 0.0
-  while div_conv <= Float::EPSILON
-    current_value = ((k.to_f) - 1) / (faculty(k).to_f)
-    div_conv = (1 - current_value).abs
+  div_conv = 1.0
+  while div_conv > Float::EPSILON
+    current_value += ((k.to_f) - 1) / ((faculty(k)).to_f)
+    div_conv = (1.0 - current_value).abs
     k += 1
+    puts(k)
   end
   {Value_float: current_value,Value_rational: current_value.to_r, Index: k}
 end
 
-puts(faculty(4))
 puts(aprox_one)
