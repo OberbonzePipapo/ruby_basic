a153950f1291ead34248c0ce37555774903866f5
Elina Eickstaedt <contact@khaleesicodes.com>
2019-10-20 12:29:02 +0200

    Correction of controlflow of 304

--- Aufgabe02/aufgabe0304.rb
+++ Aufgabe02/aufgabe0304.rb
@@ -4,13 +4,13 @@
 def russianfarme_multi(a,b)
   step_hash = Hash.new(0)
   result = 0
-  while a < 1
+  while a > 1
     step_hash[a] = b
     a = a / 2
     b = b * 2
   end
   #implent selection of b were a is even
-  step_hash.each {|k, v| if k.even?
+  step_hash.each {|k, v| if !k.even?
                            result = result + v
                          end}
   result
