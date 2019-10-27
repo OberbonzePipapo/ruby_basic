b47253e246823478d465495ada858c0a2eed4562
Elina Eickstaedt <contact@khaleesicodes.com>
2019-10-27 22:34:16 +0100

    Deleted loop, to test better

--- Aufgabe03/user_input.rb
+++ Aufgabe03/user_input.rb
@@ -13,24 +13,22 @@ class UserInput
   end
   # Method to get user input and saves it in variables to convert and check later
   def get_input
-    loop  do
-      puts("Please enter the value and the unit you want to convert from, in the format 2 cm to km. To end this converting journey, please type E.")
-      input =  gets
-      break if input.is_a?(NilClass) || (input.strip).upcase.eql?("E")
+    puts("Please enter the value and the unit you want to convert from, in the format 2 cm to km.")
+    input =  gets
+    if input.is_a?(NilClass) || (input.strip).upcase.eql?("E")
+      puts("Please restart and enter something valid")
+    else
       user_input = /^(?<value>[0-9.]+) ?(?<enteredUnit>[[:alpha:]]+)?(?:to)?(?<targetUnit>[[:alpha:]]+)?/.match(input.strip)
       @enteredUnit = user_input["enteredUnit"]
       @enteredValue = user_input["value"]
       @targetUnit = user_input["targetUnit"]
-    rescue Interrupt
-      break
     end
   end
 
-  #private
   # method which calls the right unit converter from coversion class
   def call_converter
     if LengthConversion.units.include?(@enteredUnit) && LengthConversion.units.include?(@targetUnit)
-      unit = LengthConversion.new(enteredUnit, enteredValue)
+      unit = LengthConversion.new(@enteredUnit, @enteredValue)
       puts unit.convert(@targetUnit)
     elsif MassConversion.units.include?(@enteredUnit) && MassConversion.units.include?(@targetUnit)
       unit = MassConversion.new(@enteredUnit, @enteredValue)
@@ -45,3 +43,5 @@ end
 
 result = (UserInput.new)
 result.get_input
+
+puts(result.call_converter)
