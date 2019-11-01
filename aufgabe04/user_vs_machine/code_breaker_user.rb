7a8619b1a60237a4433d7cc7b048b93d2e65feb4
Elina Eickstaedt <contact@khaleesicodes.com>
2019-11-01 19:51:10 +0100

    Adjusted User input class

--- aufgabe04/user_vs_machine/code_breaker_user.rb
+++ aufgabe04/user_vs_machine/code_breaker_user.rb
@@ -9,19 +9,8 @@ class CodeBreakerUser
     i = 1
     while i <= 4
       puts("Please enter the #{i}. number of your code")
-      number = gets.chomp
-      number.to_i
-      if number.is_a?(Integer)
-        if number < 7
-          @input << number
-          i += 1
-        else
-          raise ArgumentError, "Please enter a number between 1-6"
-        end
-      else
-        raise ArgumentError, "Please enter something valid"
-      end
-
+      @input << gets.chomp
+      i += 1
     end
     puts(@input)
    end
