83085bff57c977c92d18ff921aa8e01dc09e6bbb
Elina Eickstaedt <contact@khaleesicodes.com>
2019-11-23 20:04:41 +0100

    Intial commit of test classes

---
+++ aufgabe06/tests/test_person.rb
@@ -0,0 +1,16 @@
+# Description
+# Author:: Elina Eickstaedtrequire 'test/unit'
+
+class MyTest < Test::Unit::TestCase
+  def setup
+    # Do nothing
+  end
+
+  def teardown
+    # Do nothing
+  end
+
+  def test
+    pend 'Not implemented'
+  end
+end
